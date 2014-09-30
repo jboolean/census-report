d4d882c029040267b8dc047848b49503453541d9
Julian Boilen <julianboilen@gmail.com>
2014-09-29 20:10:36 -0400

    Updated with USA data

--- scripts/page-artistclasses.js
+++ scripts/page-artistclasses.js
@@ -58,7 +58,7 @@ YUI.add('bmp-page-artistclasses', function(Y) {
       var chart = new Y.BMP.Widget.GChart({
         chartType: 'BarChart',
         options: {
-          title: 'Approximate number of people in NYC in each creative occupation:',
+          title: 'Approximate number of people in each creative occupation:',
           height: 650,
           legend: {
             position: 'none'
